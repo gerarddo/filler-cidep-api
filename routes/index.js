c66913c250c4c0ad746ecbfa3bcbaa564dd914e8
gerarddo <germijares280995@hotmail.com>
2018-02-16 16:18:48 -0600

    fixed this i think

--- routes/index.js
+++ routes/index.js
@@ -6,38 +6,30 @@ var Point = require("../models/Point")
 
 
 router.get("/", function(req, res){
-    res.render("index", {fill: false})
+    res.send("This is the filler api working")
 }); //router.get
 
 router.post("/fill", function(req, res){
-	console.log("post request at /fill");
+	// console.log("post request at /fill");
     
-	const r = Number(req.body.r)
-	const step = Number(req.body.step)
-	const height = Number(req.body.height)
-	const heightStep = Number(req.body.heightStep)
+	// const r = Number(req.body.r)
+	// const step = Number(req.body.step)
+	// const height = Number(req.body.height)
+	// const heightStep = Number(req.body.heightStep)
 
-	// console.log("r type is" + typeof r)
-
-    var newCylinder = new Cylinder(r, step, height, heightStep).toCIDEPGcode(new Point(-30, 0 ,0), "F4")
+    // var newCylinder = new Cylinder(r, step, height, heightStep).toCIDEPGcode(new Point(-30, 0 ,0), "F4")
     
-    res.render("index", {fill: true, cylinderGcode: newCylinder})
-
-
-
-    // User.register(newUser, req.body.password, function(err, user){
-    //     if(err){
-    //         console.log(err);
-    //         return res.render("register");
-    //     }
-    //     console.log("new user created");
-    //     console.log(newUser)
-    //     passport.authenticate("local")(req, res, function(){
-    //         res.redirect("/");
-    //     }); //Passport.authenticate
-    // }); //User.register
+    // res.render("index", {fill: true, cylinderGcode: newCylinder})
+	let parameters = JSON.parse(req.body.parameters)
+	const radius = Number(parameters.radius)
+	const step = Number(parameters.step)
+	const height = Number(parameters.height)
+	const heightStep = Number(parameters.heightStep)
+    var newlyCreated = new Cylinder(radius, step, height, heightStep).toCIDEPGcode(new Point(-30, 0 ,0), "F4")
+	res.send(JSON.stringify(newlyCreated));
 }); //router.post
 
 module.exports = router;
+
 
 
